3523c64ee15e2414e01682361390021168162fbe
Sweet-love <1196940127@qq.com>
2018-12-19 21:49:55 +0800

    more

--- src/pages/home/store/actionCreators.js
+++ src/pages/home/store/actionCreators.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import * as constants from './constants';
+import { fromJS } from 'immutable';
 
 const changeHomeDate = (result) => ({
 	type: constants.CHANGE_HOME_DATE,
@@ -8,11 +9,26 @@ const changeHomeDate = (result) => ({
   articleList: result.articleList
 })
 
+const addHomeList = (list, nextPage) => ({
+  type: constants.ADD_ARICLE_LIST,
+  list: fromJS(list),
+  nextPage
+})
+
 export const getHomeInfo = ()=> {
   return (dispatch)	=> {
       axios.get('/api/home.json').then((res)=>{
         const result = res.data.data;
         dispatch(changeHomeDate(result));
       })  	
+  }
+}
+
+export const getMoreList = (page) => {
+  return (dispatch) => {
+      axios.get('/api/homeList.json?'+page).then((res)=>{
+        const result = res.data.data;
+        dispatch(addHomeList(result, page+1));
+      }) 
   }
 }
--- src/pages/home/store/constants.js
+++ src/pages/home/store/constants.js
@@ -1,1 +1,2 @@
 export const CHANGE_HOME_DATE = 'home/CHANGE_HOME_DATE';
+export const ADD_ARICLE_LIST = 'home/ADD_ARICLE_LIST';
